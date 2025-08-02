962374e7598c8e2ddc198f65775a71bf146e179e
Israel192619 <israel.zc.09@gmail.com>
2025-08-02 16:14:10 -0400

    Se crea la ruta/endpoint para login con prefix auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,4 +11,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
