0269912e0a8280f1b5f48f9add52d34f6e5001c2
Israel192619 <israel.zc.09@gmail.com>
2025-08-02 17:19:18 -0400

    Se actualiza en AuthController con los metodos me y logout

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
+        /* $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]); */
+
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
         ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
 
         // Buscar usuario
         $user = User::where('email', $request->email)->first();
@@ -35,12 +44,21 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        //dd($request->all());
         // Validar datos
-        $request->validate([
+        /* $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]); */
+        $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
 
         // Crear usuario
         $user = User::create([
@@ -57,5 +75,18 @@ class AuthController extends Controller
             'token' => $token,
             'user' => $user
         ], 201);
+    }
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logout exitoso'], 200);
+    }
+    public function me(Request $request)
+    {
+        return response()->json([
+            'user' => $request->user()
+        ]);
     }
 }
